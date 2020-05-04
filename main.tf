2b4b0edd5c09d9e76770b3fd357271c4a1ba5ce7
Krishna Sagar R <ragas.sagar.474@gmail.com>
2020-05-04 20:58:32 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,10 @@ provider "aws" {
   version = "~> 2.0"
 }
 
+variable STACK_NAME {}
+
 resource "aws_s3_bucket" "b" {
-  bucket = "sagar474mytfbucke"
+  bucket = "${var.STACK_NAME}sagar474mytfbucke"
   acl    = "private"
 
   tags = {
